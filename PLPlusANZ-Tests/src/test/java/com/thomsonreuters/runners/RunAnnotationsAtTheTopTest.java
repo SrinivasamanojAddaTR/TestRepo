43c1af15f7bbb14be876312121a95611c22a7c7a
Menta, Vineet <Vineet.Menta@thomsonreuters.com>
2021-04-05 13:10:38 +0000

    VM - ANZ - revert runner fixes
    git-tfs-id: [https://dev.azure.com/TR-Legal-Cobalt]$/Cobalt QED Testing/PLPlusANZ;C1207760

--- PLPlusANZ-Tests/src/test/java/com/thomsonreuters/runners/RunAnnotationsAtTheTopTest.java
+++ PLPlusANZ-Tests/src/test/java/com/thomsonreuters/runners/RunAnnotationsAtTheTopTest.java
@@ -12,8 +12,8 @@ import org.slf4j.LoggerFactory;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber-htmlreport/AnnotationsAtTheTopReport", "junit:target/junit_cucumber.xml", "json:target/json-files/RunAnnotationsAtTheTopTest.json", "com.epam.reportportal.cucumber.ScenarioReporter", "rerun:target/ReRunAnnotationsAtTheTop.txt"},
-        features = "classpath:com/thomsonreuters/features/annotations/top",
+        plugin = {"pretty", "html:target/cucumber-htmlreport/AnnotationsAtTheTopReport", "junit:target/junit_cucumber.xml", "json:target/json-files/RunAnnotationsAtTheTopTest.json", "com.epam.reportportal.cucumber.ScenarioReporter"},
+        features = "src/test/resources/com/thomsonreuters/features/annotations/top",
         tags = {"~ ", "~@manual"},
         glue = {"com.thomsonreuters.step_definitions", "com.thomsonreuters.hooks"},
         monochrome = true,
