7bcdab19f9f29d3e87e69f653dcfc27c623e13d0
c260892 <c260892@ead.tfs.local>
2021-04-23 05:18:28 +0000

    PLAU password update
    git-tfs-id: [https://dev.azure.com/TR-Legal-Cobalt]$/Cobalt QED Testing/PLPlusANZ;C1211217

--- PLPlusANZ-Tests/src/test/java/com/thomsonreuters/runners/RunHeaderTest.java
+++ PLPlusANZ-Tests/src/test/java/com/thomsonreuters/runners/RunHeaderTest.java
@@ -26,7 +26,7 @@ public class RunHeaderTest extends BaseCucumberTestRunner {
     public static void reporting() {
         if (System.getProperty("username","None").equals("None")) {
             User.getInstance().setUserName("AUtestuser9");
-            User.getInstance().setPassword("P@ssword1");
+            User.getInstance().setPassword("P@ssword2");
             LOG.info("The credentials have been set");
         }
         else {
