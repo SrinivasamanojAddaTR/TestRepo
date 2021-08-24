32f655f3d8d1b9002a6c83b0ced17e03ea038abc
c260892 <c260892@ead.tfs.local>
2021-08-24 05:48:21 +0000

    PLAU Contact list xpath fix
    git-tfs-id: [https://dev.azure.com/TR-Legal-Cobalt]$/Cobalt QED Testing/PLPlusANZ;C1232080

--- PPIAU-PageObjects/src/main/java/com/thomsonreuters/pageobjects/pages/annotations/ContactsForSharingPage.java
+++ PPIAU-PageObjects/src/main/java/com/thomsonreuters/pageobjects/pages/annotations/ContactsForSharingPage.java
@@ -4,7 +4,7 @@ import com.thomsonreuters.driver.framework.AbstractPage;
 import org.openqa.selenium.*;
 
 public class ContactsForSharingPage extends AbstractPage {
-    public static final String SEARCH_RESULT_CHECKBOX_PATH = "//*[@id='coid_contacts_peopleListItems_contacts_edit']//*[@role='checkbox' and contains(text(),%s)]]";
+    public static final String SEARCH_RESULT_CHECKBOX_PATH = "//*[@id='coid_contacts_peopleListItems_contacts_edit']//*[@role='checkbox' and contains(text(),'%s')]";
     public static final String CHECKBOX_PATH = "//*[@role='checkbox' and contains(text(),'%s')]";
 
     public WebElement contactsLink() {
